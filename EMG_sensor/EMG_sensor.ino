b2a9c4dc522da8573b8d8ec8ee517c77abcb1057
chrismcpr <144912285+chrismcpr@users.noreply.github.com>
2023-11-30 21:26:12 -0500

    Add files via upload

--- EMG_sensor/EMG_sensor.ino
+++ EMG_sensor/EMG_sensor.ino
@@ -6,6 +6,11 @@
 #define potentiometer 26
 RH_NRF24 nrf24(CE,CSN);
 
+struct Data {
+  int int1;
+  int int2;
+} dataToSend;
+
 void setup() {
   Serial.begin(9600);
 
@@ -71,9 +76,14 @@ void loop() {
   char msg[16];
   itoa(sensorValue, msg, 10);
 
-  nrf24.send((uint8_t*)msg, strlen(msg));
-  nrf24.send((uint8_t*)leftMsg, strlen(leftMsg));
-  nrf24.send((uint8_t*)rightMsg, strlen(rightMsg));
+  dataToSend.int1 = leftMotor;
+  dataToSend.int2 = rightMotor;
+
+  nrf24.send((uint8_t*)&dataToSend, sizeof(Data));
+
+  //nrf24.send((uint8_t*)msg, strlen(msg));
+  //nrf24.send((uint8_t*)leftMsg, strlen(leftMsg));
+  //nrf24.send((uint8_t*)rightMsg, strlen(rightMsg));
   nrf24.waitPacketSent();
 
   delay(100);
