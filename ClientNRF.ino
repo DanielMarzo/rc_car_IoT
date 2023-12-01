163f2f0a93389aca9a5fa10749a3f2c02f0fcc28
RodolfoDA22 <72635950+RodolfoDA22@users.noreply.github.com>
2023-11-30 19:30:13 -0500

    Updated ClientNRF.ino
    Now with Left and Right Motor Steering, work in progress

--- ClientNRF.ino
+++ ClientNRF.ino
@@ -1,4 +1,3 @@
- 
 #include <SPI.h>
 #include <RH_NRF24.h>
 #ifdef ESP32
@@ -11,14 +10,22 @@
 RH_NRF24 nrf24(CE, CSN);
 //RH_ASK driver(2000, 4, 0, 2); // ESP8266 or ESP32: do not use pin 11 or 2
 
-const int potLeftPin = 33; // Connect to the wiper of the potentiometer
-const int potRightPin = 34; // Connect to the wiper of the potentiometer
+// const int potLeftPin = 33; // Connect to the wiper of the potentiometer
+// const int potRightPin = 34; // Connect to the wiper of the potentiometer
 
-const int leftMotor = 25;
-const int rightMotor = 26;
+//byte motorMovement[2];
+
+struct Data {
+    int leftMotor;
+    int rightMotor;
+} receivedData;
+
+const int leftMotor = 14;
+const int rightMotor = 12;
 
 const int freq = 2000;
-const int motorChannel = 0;
+const int motorChannelLeft = 0;
+const int motorChannelRight = 1;
 const int resolution = 12;
 
  
@@ -38,47 +45,137 @@ void setup()
     Serial.println("setRF failed");    
   }
 
-  pinMode(potLeftPin, INPUT);
-  pinMode(potRightPin, INPUT);
-  //pinMode(leftMotor, OUTPUT);
+  ledcSetup(motorChannelLeft, freq, resolution);
+  ledcAttachPin(leftMotor, motorChannelLeft);
 
-  ledcSetup(motorChannel, freq, resolution);
-  ledcAttachPin(leftMotor, motorChannel);
-  ledcAttachPin(rightMotor, motorChannel);
+  ledcSetup(motorChannelRight, freq, resolution);
+  ledcAttachPin(rightMotor, motorChannelRight);
 }
  
  
 void loop()
 {
-  Serial.println("Sending to nrf24_server");
+  //Serial.println("Sending to nrf24_server");
   // Send a message to nrf24_server
-   uint8_t data[] = "This is the Receiver.";
+  //uint8_t data[] = "This is the Receiver.";
   //uint8_t data[] = "blink";
-  nrf24.send(data, sizeof(data));
-  
-  nrf24.waitPacketSent();
-
-
+  //nrf24.send(data, sizeof(data));
+  //nrf24.waitPacketSent();
 
   // Now wait for a reply
-  uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
+  // uint8_t buf[RH_NRF24_MAX_MESSAGE_LEN];
+  // uint8_t len = sizeof(buf);
+
+
+  //nrf24.read(movement, sizeof(movement));
+
  
   if (nrf24.waitAvailableTimeout(500)) { 
     // Should be a reply message for us now   
-    if (nrf24.recv(buf, &len)){
+    //if (nrf24.recv(buf, &len)){
+    //if (nrf24.read(movement, sizeof(movement))){ //Trying to receive data as an array
+    if (read(&receivedData, sizeof(Data))){ // usign objects now????
+
+      //og
       //Serial.print("Speed received: ");
-      Serial.println(atoi((char*)buf));
-      int speed = map(atoi((char*)buf), 0, 4095, 0, 4095);
-      if(speed >= 1600){
-        speed += 1500;
-        if(speed > 4095) {
-          speed = 4095;
+      //Serial.println(atoi((char*)buf));
+
+      //array
+      // int valLeftMotor = atoi((char*)movement[0]);
+      // int valRightMotor = atoi((char*)movement[1]);
+
+      //object
+      Serial.print("Received integers: ");
+      Serial.print(receivedData.leftMotor);
+      Serial.print(", ");
+      Serial.println(receivedData.rightMotor);
+
+      //og
+      // Serial.print("EMG VAL: ");
+      // Serial.println(emgVal);
+
+      //array
+      // Serial.print("Left VAL: ");
+      // Serial.println(valLeftMotor);
+      // Serial.print("Right VAL: ");
+      // Serial.println(valRightMotor);
+
+      //int speed = map(atoi((char*)buf), 0, 4095, 0, 4095);
+      //Serial.println(speed);
+
+      //og
+      // if(emgVal > 500){
+      //   //emgVal += 2000;
+      //   if(emgVal > 4095) {
+      //     emgVal = 4095;
+      //   }
+      //   int speed = map(emgVal, 0, 4095, 0, 4095);
+      //   Serial.print("Speed: ");
+      //   Serial.println(speed);
+      //   ledcWrite(motorChannel, speed);
+      // }
+      // else{
+      //    ledcWrite(motorChannel, 0);
+      // }
+
+      //object
+      if(receivedData.leftMotor > 500 && receivedData.rightMotor > 500){ //both motors greater than 500
+
+        receivedData.leftMotor += 2000;
+        receivedData.rightMotor += 2000;
+
+        if(receivedData.leftMotor > 4095) {
+          receivedData.leftMotor = 4095;
         }
-        ledcWrite(motorChannel, speed);
+        if(receivedData.rightMotor > 4095) {
+          receivedData.rightMotor = 4095;
+        }
+
+        Serial.print("Left Motor: ");
+        Serial.print(receivedData.leftMotor);
+        Serial.print(" | ");
+        Serial.print("Right Motor: ");
+        Serial.println(receivedData.rightMotor);
+        ledcWrite(motorChannelLeft, receivedData.leftMotor);
+        ledcWrite(motorChannelRight, receivedData.rightMotor);
+      }
+      else if(receivedData.leftMotor < 500 && receivedData.rightMotor > 500){ //left motor less than 500; right, greater
+
+        receivedData.leftMotor = 0;
+        receivedData.rightMotor += 2000;
+
+        if(receivedData.rightMotor > 4095) {
+          receivedData.rightMotor = 4095;
+        }
+
+        Serial.print("Left Motor: ");
+        Serial.print(receivedData.leftMotor);
+        Serial.print(" | ");
+        Serial.print("Right Motor: ");
+        Serial.println(receivedData.rightMotor);
+        ledcWrite(motorChannelLeft, receivedData.leftMotor);
+        ledcWrite(motorChannelRight, receivedData.rightMotor);
+      }
+      else if(receivedData.leftMotor > 500 && receivedData.rightMotor < 500){ //right motor less than 500; left, greater
+
+        receivedData.leftMotor += 2000;
+        receivedData.rightMotor = 0;
+
+        if(receivedData.leftMotor > 4095) {
+          receivedData.leftMotor = 4095;
+        }
+
+        Serial.print("Left Motor: ");
+        Serial.print(receivedData.leftMotor);
+        Serial.print(" | ");
+        Serial.print("Right Motor: ");
+        Serial.println(receivedData.rightMotor);
+        ledcWrite(motorChannelLeft, receivedData.leftMotor);
+        ledcWrite(motorChannelRight, receivedData.rightMotor);
       }
       else{
-         ledcWrite(motorChannel, 0);
+        ledcWrite(motorChannelLeft, 0);
+        ledcWrite(motorChannelRight, 0);
       }
       
     }
